3955b881a5c91125c525b01027950fe84c1bb747
Zhouyongkan <1033378094@qq.com>
2020-04-21 18:55:31 +0800

    字符串中的第一个唯一字符

---
+++ 字符串中的第一个唯一字符.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+	int firstUniqChar(string s) {
+		int count[256] = { 0 };
+		int size = s.size();
+		for (int i = 0; i<size; ++i)
+		{
+			count[s[i]] += 1;
+		}
+		for (int i = 0; i<size; ++i)
+		{
+			if (count[s[i]] == 1)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+};
