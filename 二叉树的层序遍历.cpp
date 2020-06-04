c12e282b83120cf3271b3b3bc44b6cd08ed3af70
Zhouyongkan <1033378094@qq.com>
2020-06-04 20:48:04 +0800

    给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。

---
+++ 二叉树的层序遍历.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+	vector<vector<int>> levelOrder(TreeNode* root) {
+		vector<vector<int>> vv;
+
+		queue<TreeNode*> q;
+		if (root)
+			q.push(root);
+
+		while (!q.empty())
+		{
+			vector<int> v;
+			// 队列中的结点个数是每一层的个数,第一次是第一层的，只有一个
+			int level = q.size();
+
+			// 将当前层的数据都出来，放到v中，再将下一层的都入到队列中
+			for (size_t i = 0; i < level; ++i)
+			{
+				TreeNode* front = q.front();
+				q.pop();
+				v.push_back(front->val);
+				if (front->left)
+					q.push(front->left);
+
+				if (front->right)
+					q.push(front->right);
+			}
+
+			vv.push_back(v);
+		}
+
+		return vv;
+	}
+};
