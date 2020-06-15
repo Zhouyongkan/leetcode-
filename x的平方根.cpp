3098031ae4007d5e132a341af282fd3fbd7cd9a9
Zhouyongkan <1033378094@qq.com>
2020-06-15 22:28:35 +0800

    实现 int sqrt(int x) 函数。
    计算并返回 x 的平方根，其中 x 是非负整数。
    
    由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。

---
+++ x的平方根.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+	int mySqrt(int x) {
+		int low = 0;
+		int high = x;
+		int mid = 0;
+		if (x < 0)
+		{
+			return -1;
+		}
+		if (x <= 1)
+		{
+			return x;
+		}
+		while (low + 1 < high)
+		{
+			mid = low + (high - low) / 2;
+
+			if (x / mid < mid)
+			{
+				high = mid;
+			}
+			else
+			{
+				low = mid;
+			}
+		}
+		return low;
+	}
+};
